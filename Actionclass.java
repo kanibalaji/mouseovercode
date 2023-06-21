ec674dc2db38ab1339ff3297bee0f5522a26eebb
unknown <kanibalaji16@gmail.com>
2023-06-21 20:43:20 +0530

    initial commit

---
+++ Actionclass.java
@@ -0,0 +1,33 @@
+package org.actioncode1;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+public class Actionclass {
+
+	public static void main(String[] args) throws IOException, InterruptedException{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(options);
+		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.switchTo().frame(0);
+		WebElement source=driver.findElement(By.id("draggable"));
+		WebElement target=driver.findElement(By.id("droppable"));
+		Actions builder=new Actions(driver);
+		 Thread.sleep(7000);
+		builder.dragAndDrop(source,target).perform();
+		Thread.sleep(7000);
+		
+		
+	}
+
+}
